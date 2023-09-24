281f673d02f6bedab26a7bb7344d7961154b5d45
rnferrer <rnferrer@ucdavis.edu>
2023-09-23 21:09:17 -0700

    implemented callback route and received tokens from spotify

--- app/api/callback/route.ts
+++ app/api/callback/route.ts
@@ -1,4 +1,32 @@
-import { NextApiRequest } from "@/node_modules/next/dist/shared/lib/utils"
-export const GET = (request: NextApiRequest) => {
+import { spotifyApi } from "@/utils/spotifyAuth"
+import { useRouter } from "@/node_modules/next/router";
+
+import { NextApiRequest, NextApiResponse } from "@/node_modules/next/dist/shared/lib/utils"
+
+export const GET = async (request: NextApiRequest, response: NextApiResponse) => {
   
+  if (typeof request.url === 'string'){
+    const url: string | URL  = new URL(request.url, 'http://localhost:3000'); // Replace with your actual base URL
+    const code = url.searchParams.get('code');
+    
+    //code is not given in request
+    if (!code){
+      return response.status(400).send('Missing authorization code');
+    }
+
+    try {
+      const data = await spotifyApi.authorizationCodeGrant(code);
+      const {access_token, refresh_token} = data.body;
+      response.setHeader('Location', '/dashboard');
+      response.status(302).end();
+    }
+    catch(e){
+      console.log(e)
+      return response.send({message: 'error'})
+    }
+  }
+  else{
+    return response.status(400).send('Invalid request.')
+  }
+
 }
