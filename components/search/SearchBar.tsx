641e248543901ee98cf466bab20d98c4569149ee
rnferrer <rnferrer@ucdavis.edu>
2023-11-30 14:53:22 +0900

    added debounce function to prevent too many search calls

--- components/search/SearchBar.tsx
+++ components/search/SearchBar.tsx
@@ -23,32 +23,49 @@ export const colorTheme = createTheme({
 
 const SearchBar = ():JSX.Element => {
   
-  const [search, setSearch] = useState('')
-  const [results, setResults] = useState([])
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
 
-  //need to implement using debounce to delay too many simultaneous requests
-  const handleChange = async(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) =>{
-    event.preventDefault()
-    setSearch(event.target.value)
-    
-    if (search.replace(/\s/g, '').length === 0){
-      setResults([])
-    }
-    else{
-      const queryString = new URLSearchParams({searchQuery: search}).toString();
-      const url = `/api/spotify/search?${queryString}`;
-      const response = await fetch (url);
-      let data = await response.json()
-      setResults(data)
-      console.log(data)
-    }
+  const debounce = (func: Function, delay: Number) => {
+    let timeoutId: NodeJS.Timeout;
+
+    return function () {
+      const context = this;
+      const args = arguments;
+
+      clearTimeout(timeoutId);
+
+      timeoutId = setTimeout(() => {
+        func.apply(context, args);
+      }, delay);
+    };
+  };
+
+  const handleSearch = debounce(async() => {
+    console.log(`Searching for '${search}'`)
+      if (search.replace(/\s/g, '').length === 0){
+        setResults([])
+      }
+      else{
+        const queryString = new URLSearchParams({searchQuery: search}).toString();
+        const url = `/api/spotify/search?${queryString}`;
+        const response = await fetch (url);
+        let data = await response.json()
+        setResults(data)
+        console.log(data)
+      }
+  }, 1000)
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { value } = event.target;
+    setSearch(value);
+    handleSearch();
   }
-
 
   return(
     <ThemeProvider theme={colorTheme}>
-      <div className="w-full h-auto flex justify-center items-center">
-        <div className="flex flex-col w-1/2 items-center absolute">
+      <div className="w-full h-auto flex justify-center items-center relative">
+        <div className="flex flex-col w-1/2 items-center fixed top-1/3 left-1/4">
           <TextField 
           color="primary"
           className="w-full"
@@ -63,9 +80,11 @@ const SearchBar = ():JSX.Element => {
           </>
           :
           <List sx={{ width: '100%', bgcolor: 'background.gray' }}>
-            {results.map((result) => (
+            {(results.length > 0)?
+         
+            results.map((result, index) => (
               <ListItem
-                key={result}
+                key={index}
                 disableGutters
                 disablePadding={true}
               >
@@ -80,7 +99,10 @@ const SearchBar = ():JSX.Element => {
 
                 </ListItemButton>
               </ListItem>
-            ))}
+            ))
+            :
+            <></>
+            }
           </List>
 
           }
